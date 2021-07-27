dfbf0ebfb5f038693ad40d1474dad70622a1d5c9
Andrea Facchini <andrea.facchini.ext@orange.com>
2021-07-27 15:45:38 +0200

    allow ng testing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { LoginUserDto } from 'src/user/dto/login-user.dto';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { toUserDto, UserDto } from 'src/user/dto/user.dto';
 import { checkPassword, hashPassword } from './auth-helper';
+import { JwtToken } from './interfaces/jwt-token';
 
 @Injectable()
 export class AuthService {
@@ -20,15 +21,15 @@ export class AuthService {
     return user;
   }
 
-  async login(loginUserDto: LoginUserDto): Promise<string> {
-    console.log(loginUserDto)
+  async login(loginUserDto: LoginUserDto): Promise<JwtToken> {
+    console.log(loginUserDto);
     return this.userService
       .findOneLogin(loginUserDto.email)
       .then(async (user) => {
-        console.log(user)
+        console.log(user);
         const res = await checkPassword(loginUserDto.password, user.password);
         if (res) {
-          return this.signToken(toUserDto(user));
+          return { token: this.signToken(toUserDto(user)) };
         } else {
           throw new UnauthorizedException();
         }
